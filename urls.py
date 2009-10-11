5e6128d8fcc2f6ed5e9e5333e95a397c4c5b7dc9
Alec Perkins <alecperkins@gmail.com>
2009-10-11 16:25:54 -0400

    Fixed urls to pass "slug" instead of "name"

--- urls.py
+++ urls.py
@@ -22,8 +22,10 @@ urlpatterns = patterns('',
 
     #strands/knots
     url(r'^strands/$', 'strands.views.tapestry', name='tapestry'),
-    url(r'^strands/(?P<strand_a_name>[\w\._-]+)/$', 'strands.views.display_strand', name='display_strand_by_a'),
-    url(r'^strands/(?P<strand_a_name>[\w\._-]+)/(?P<strand_b_name>[\w\._-]+)/$', 'strands.views.display_strand', name='display_strand_by_a_b'),
-    url(r'^author/(?P<author_username>[\w\._-]+)/', 'strands.views.display_by_author', name='display_strand_by_author'),
+    url(r'^strands/(?P<strand_a_slug>[\w\._-]+)/$', 'strands.views.display_strand', name='display_strand_by_a'),
+    url(r'^strands/(?P<strand_a_slug>[\w\._-]+)/(?P<strand_b_slug>[\w\._-]+)/$', 'strands.views.display_strand', name='display_strand_by_a_b'),
+    url(r'^author/(?P<author_username>[\w\._-]+)/', 'strands.views.display_by_author', name='display_by_author'),
+    url(r'^author/(?P<author_username>[\w\._-]+)/(?P<order>[\w\._-]+)$', 'strands.views.display_by_author', name='display_by_author_order'),
     url(r'^knot/(?P<knot_slug>[\w\._-]+)/$', 'strands.views.display_knot', name='display_knot'),
+    #url(r'^date/(?P<date_slug>[\w\._-]+)/$', 'strands.views.display_by_date', name='display_by_date'),
 )
