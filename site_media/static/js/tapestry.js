f083ed67fcb6b4905b4825b563ff02e22d8b8c82
Alec Perkins <alecperkins@gmail.com>
2011-01-22 01:54:10 -0500

    ...

---
+++ site_media/static/js/tapestry.js
@@ -0,0 +1,3 @@
+render data and settings to js variables in template
+initiate tapestry using settings {} and variables
+draw tapestry
--- views.py
+++ views.py
@@ -1,6 +1,7 @@
 from django.conf import settings
 from django.http import  HttpResponseRedirect
 from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def favicon_redirect(request):
     return HttpResponseRedirect('%simages/favicon.png' % settings.STATIC_URL)
@@ -8,6 +9,9 @@ def favicon_redirect(request):
 def toggle_mobile(request):
     request.session['is_mobile'] = not request.session.get('is_mobile', True)
     return HttpResponseRedirect(request.META.get('HTTP_REFERER', reverse('home')))
+
+
+from strands_core.models import Knot
 
 def sitemap(request, template_name="sitemap.xml.django"):
     knots = Knot.objects.filter(published=True).order_by("-date")
